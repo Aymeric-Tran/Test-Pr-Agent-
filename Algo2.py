73e0400d76da5670ab19d70855d20a74c1be524b
Aymeric-Tran <aymeric.tran@axa.com>
2024-08-20 15:56:06 +0200

    Update Algo2.py

--- Algo2.py
+++ Algo2.py
@@ -1,9 +1,7 @@
 def tri_insertion(tableau):
     for i in range(1, len(tableau)):
         en_cours = tableau[i]
-        j = i
-        while j > 0 and tableau[j-1] > en_cours:
-            tableau[j] = tableau[j-1]
+        j
             j -= 1
         tableau[j] = en_cours
     return tableau
