c89fbe4d130f50dd616a7ca59e3187e499f5c81d
Aymeric-Tran <aymeric.tran@axa.com>
2024-08-20 15:52:20 +0200

    Update Algo1.py

--- Algo1.py
+++ Algo1.py
@@ -1,9 +1,7 @@
 def tri_selection(tableau):
     for i in range(len(tableau)):
-        mini = i
+        mini = 
         for j in range(i+1, len(tableau)):
             if tableau[j] < tableau[mini]:
                 mini = j
-        # On échange les éléments            
-        tableau[mini], tableau[i] = tableau[i], tableau[mini]
-    return tableau
+
